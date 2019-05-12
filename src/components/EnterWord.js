3452201890716ad3d2d9c397e7df33a6720830bb
Eric Gibby <eric.gibby@imaginelearning.com>
2019-05-12 12:57:59 -0600

    Added submit button to `EnterWord` form

--- src/components/EnterWord.js
+++ src/components/EnterWord.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './EnterWord.scss';
 
 function EnterWord(props) {
 	const [word, wordChanged] = useState('');
@@ -14,11 +15,21 @@ function EnterWord(props) {
 
 	return (
 		<div className="EnterWord">
+			<p>Enter a word or phrase to guess.</p>
 			<form onSubmit={handleSubmit}>
-				<label>
-					Enter a word to guess:
-					<input type="password" placeholder="Enter a word" value={word} onChange={handleChange} />
-				</label>
+				<div className="grid-x">
+					<div className="cell medium-10 small-12">
+						<label>
+							<span className="show-for-sr">Enter a word</span>
+							<input type="password" placeholder="Enter a word" value={word} onChange={handleChange} />
+						</label>
+					</div>
+					<div className="cell medium-2 small-12">
+						<button type="submit" className="button primary">
+							OK
+						</button>
+					</div>
+				</div>
 			</form>
 		</div>
 	);
