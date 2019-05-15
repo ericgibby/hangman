8adefe1f34d13be56149ee8e5039a0d982fba52b
Eric Gibby <eric.gibby@imaginelearning.com>
2019-05-14 22:23:29 -0600

    Updates

---
+++ precache-manifest.d8ba3f6ffb8efc19d32c0219e0a49e22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7ad5e54fac858a2cdf96f6b987e173",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "e235e1975ce111827794",
+    "url": "/hangman/static/css/main.726b5c03.chunk.css"
+  },
+  {
+    "revision": "c2258ad158450285dcc1",
+    "url": "/hangman/static/js/2.b668eb41.chunk.js"
+  },
+  {
+    "revision": "e235e1975ce111827794",
+    "url": "/hangman/static/js/main.8e6a985b.chunk.js"
+  },
+  {
+    "revision": "194922e750fb807a0912",
+    "url": "/hangman/static/js/runtime~main.13e36f11.js"
+  }
+]);
