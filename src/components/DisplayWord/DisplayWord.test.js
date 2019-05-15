798184824e49cff7ddb7426249b698fa422efa0c
Eric Gibby <eric.gibby@imaginelearning.com>
2019-05-14 22:56:06 -0600

    Fixed `getCharacters` test

--- src/components/DisplayWord/DisplayWord.test.js
+++ src/components/DisplayWord/DisplayWord.test.js
@@ -3,17 +3,17 @@ import { getCharacters } from './DisplayWord';
 describe('DisplayWord component', () => {
 	describe('getCharacters(...)', () => {
 		it('inserts underscores for letters', () => {
-			const characters = getCharacters('HELLO', []);
+			const characters = getCharacters('HELLO', {});
 			expect(characters).toBe('_____');
 		});
 
 		it('does not insert underscores for non-letters', () => {
-			const characters = getCharacters('HELLO WORLD!', []);
+			const characters = getCharacters('HELLO WORLD!', {});
 			expect(characters).toBe('_____ _____!');
 		});
 
 		it('shows letters that exist in the specified array', () => {
-			const characters = getCharacters('HELLO WORLD!', ['L', 'O']);
+			const characters = getCharacters('HELLO WORLD!', { L: true, O: true });
 			expect(characters).toBe('__LLO _O_L_!');
 		});
 	});
